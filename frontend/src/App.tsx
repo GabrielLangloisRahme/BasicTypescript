6bdf15221d0173ee111e783bd8f4b8a8452590ee
GabrielLangloisRahme <gabriel.langloisrahme@mail.mcgill.ca>
2020-04-08 13:25:53 -0400

    Finished Work Order Page and put boiler plate code for details page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,16 +14,22 @@ function Home() {
 }
 
 interface WorkOrdersState {
-  workOrders: {
-    id: number;
-    name: string;
-    status: string;
-  } | null;
+  workOrders:
+    | [
+        {
+          id: number;
+          name: string;
+          status: string;
+        }
+      ]
+    | null;
+  workOrdersExist: boolean;
 }
 
 class WorkOrders extends Component<any, WorkOrdersState> {
   public state: WorkOrdersState = {
     workOrders: null,
+    workOrdersExist: true,
   };
 
   async componentWillMount() {
@@ -37,15 +43,32 @@ class WorkOrders extends Component<any, WorkOrdersState> {
     const jsonResponse = await res.json();
 
     this.setState({ workOrders: jsonResponse.workOrders });
+    this.setState({ workOrdersExist: jsonResponse.workOrders ? true : false });
   }
 
   render() {
     const { workOrders } = this.state;
-    {
-      console.log("this is the data structure", workOrders);
+    let displayWorkOrders;
+    let url = "/workOrderDetail/";
+
+    if (workOrders) {
+      displayWorkOrders = workOrders.map(({ id, name, status }) => {
+        let linkUrl = url.concat(id.toString());
+        return (
+          <p>
+            Work order {name} has a stutus of {status}. Click{" "}
+            <Link to={linkUrl}>here</Link> for details
+          </p>
+        );
+      });
+    } else if (this.state.workOrdersExist) {
+      displayWorkOrders = <p></p>;
+    } else {
+      displayWorkOrders = <p>There are no work orders at this time</p>;
     }
     return (
       <div>
+        {displayWorkOrders}
         {/* {!!favorite && (
           <p>
             My favorite Morty is <strong>{favorite.name}</strong>!
@@ -75,6 +98,40 @@ class WorkOrders extends Component<any, WorkOrdersState> {
   // };
 }
 
+interface WorkOrderDetailState {
+  workOrderDetail:
+    | [
+        {
+          name: string;
+          status: string;
+          assignee: string;
+          email: string;
+        }
+      ]
+    | null;
+}
+
+class WorkOrderDetail extends Component<any, WorkOrderDetailState> {
+  public state: WorkOrderDetailState = {
+    workOrderDetail: null,
+  };
+
+  async componentWillMount() {
+    // let res = await fetch("/api/workOrders", {
+    //   method: "GET",
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json",
+    //   },
+    // });
+    // const jsonResponse = await res.json();
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
 export default class App extends Component {
   render() {
     return (
@@ -84,6 +141,7 @@ export default class App extends Component {
           <div className="app-body">
             <Route exact path="/" component={Home} />
             <Route path="/workOrders" component={WorkOrders} />
+            <Route path="/workOrderDetail/:id" component={WorkOrderDetail} />
           </div>
         </div>
       </Router>
