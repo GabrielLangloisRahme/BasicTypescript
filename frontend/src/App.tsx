e27c2b755bb88bef5cbd1341e24baaf69fcdb766
GabrielLangloisRahme <gabriel.langloisrahme@mail.mcgill.ca>
2020-04-08 11:54:58 -0400

    Gathered state from back end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,54 +7,72 @@ function Home() {
   return (
     <>
       <p>
-        Take me to the <Link to="/example">Example Page</Link>.
+        Take me to the <Link to="/workOrders">Work Order Page</Link>.
       </p>
     </>
   );
 }
 
-interface IExampleState {
-  favorite: {
+interface WorkOrdersState {
+  workOrders: {
     id: number;
     name: string;
-    email: string;
+    status: string;
   } | null;
 }
 
-class Example extends Component<any, IExampleState> {
-  public state: IExampleState = {
-    favorite: null,
+class WorkOrders extends Component<any, WorkOrdersState> {
+  public state: WorkOrdersState = {
+    workOrders: null,
   };
 
-  render() {
-    const { favorite } = this.state;
-
-    return (
-      <div>
-        {!!favorite && (
-          <p>
-            My favorite Morty is <strong>{favorite.name}</strong>!
-          </p>
-        )}
-        <button type="button" onClick={this.handleHelloWorld}>
-          Who's my favorite Morty?
-        </button>
-      </div>
-    );
-  }
-
-  private handleHelloWorld = async () => {
-    const response = await fetch("/api/example", {
-      method: "POST",
+  async componentWillMount() {
+    let res = await fetch("/api/workOrders", {
+      method: "GET",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ id: Math.floor(1 + Math.random() * 9) }),
     });
-    const jsonResponse = await response.json();
-    this.setState({ favorite: jsonResponse.favorite });
-  };
+    const jsonResponse = await res.json();
+
+    this.setState({ workOrders: jsonResponse.workOrders });
+  }
+
+  render() {
+    const { workOrders } = this.state;
+    {
+      console.log("this is the data structure", workOrders);
+    }
+    return (
+      <div>
+        {/* {!!favorite && (
+          <p>
+            My favorite Morty is <strong>{favorite.name}</strong>!
+          </p>
+        )}
+        <p>
+          This button{" "}
+          <button type="button" onClick={this.handleHelloWorld}>
+            Who's my favorite Morty?
+          </button>
+        </p> */}
+      </div>
+    );
+  }
+
+  // private handleHelloWorld = async () => {
+  //   const response = await fetch("/api/example", {
+  //     method: "POST",
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ id: Math.floor(1 + Math.random() * 9) }),
+  //   });
+  //   const jsonResponse = await response.json();
+  //   this.setState({ favorite: jsonResponse.favorite });
+  // };
 }
 
 export default class App extends Component {
@@ -65,7 +83,7 @@ export default class App extends Component {
           <img src={logo} className="app-logo" alt="logo" />
           <div className="app-body">
             <Route exact path="/" component={Home} />
-            <Route path="/example" component={Example} />
+            <Route path="/workOrders" component={WorkOrders} />
           </div>
         </div>
       </Router>
